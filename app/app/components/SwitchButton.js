1c87ff4e8fc17d33813766b51d321236912c2473
Eduardo Vieira <edusvieirap@gmail.com>
2019-08-16 12:07:58 -0300

    Changing buttons on Quiz.js

--- app/app/components/SwitchButton.js
+++ app/app/components/SwitchButton.js
@@ -6,50 +6,45 @@ export default class SwitchButton extends Component {
 
   constructor(props) {
     super(props);
-    this.state = { simStatus: false, naoStatus: false };
-  }
-
-  updateStatus() {
-    this.setState({simStatus: !this.state.simStatus});
-    this.setState({naoStatus: this.state.simStatus})
+    this.state = { value: false };
   }
 
   render () {
     return (
       <View style={button.container}>
 
-        <LinearGradient 
-          style={button.border} 
-          start={{x: 1, y: 0}} 
-          end={{x: 0, y: 1}} 
+        <LinearGradient
+          style={button.border}
+          start={{x: 1, y: 0}}
+          end={{x: 0, y: 1}}
           colors={['#EF5350', '#F59896']}>
-          <TouchableOpacity 
-            style={this.state.simStatus ? button.empty : button.content}
-            onPress={this.updateStatus.bind(this)}>
-            <Text style={this.state.simStatus ? button.textPress : button.text}>Sim</Text>
+          <TouchableOpacity
+            style={this.state.value ? button.empty : button.content}
+            onPress={() => this.setState({value: true})}>
+            <Text style={this.state.value ? button.textPress : button.text}>Sim</Text>
           </TouchableOpacity>
         </LinearGradient>
 
-        <LinearGradient 
-          style={button.border} 
-          start={{x: 1, y: 0}} 
-          end={{x: 0, y: 1}} 
+        <LinearGradient
+          style={button.border}
+          start={{x: 1, y: 0}}
+          end={{x: 0, y: 1}}
           colors={['#EF5350', '#F59896']}>
-          <TouchableOpacity 
-            style={this.state.naoStatus ? button.empty : button.content}
-            onPress={this.updateStatus.bind(this)}>
-            <Text style={this.state.naoStatus ? button.textPress : button.text}>Não</Text>
+          <TouchableOpacity
+            style={!this.state.value ? button.empty : button.content}
+            onPress={() => this.setState({value: false})}>
+            <Text style={!this.state.value ? button.textPress : button.text}>Não</Text>
           </TouchableOpacity>
         </LinearGradient>
       </View>
-      
+
     );
   }
 }
 
 
 const button = StyleSheet.create({
-  container: {    
+  container: {
     flex: 1,
     flexDirection: 'row',
     justifyContent: 'center',
